092a901b5282b3044149952efc46da2df574a7c7
cesell <csell@outlook.com>
2020-05-24 23:44:49 -0600

    First Commit

---
+++ capstone/main.py
@@ -0,0 +1,12 @@
+#%% [markdown]
+# ## Capstone Project
+# This notebook will be used for the Applied Data Science Capstone Project
+
+# %%
+import pandas as pd
+import numpy as np
+
+# %%
+print('Hello Capstone Project Course!')
+
+# %%
